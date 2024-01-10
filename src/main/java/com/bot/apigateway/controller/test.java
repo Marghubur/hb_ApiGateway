05794802cdc040b475fcc21c1dee7bd75469bced
istiyakmi9 <istiyaq.mi9@gmail.com>
2024-01-10 16:56:04 +0530

    path improved

--- src/main/java/com/bot/apigateway/controller/test.java
+++ src/main/java/com/bot/apigateway/controller/test.java
@@ -5,9 +5,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/api/gateway/")
+@RequestMapping(value = "/hb/api/gateway/")
 public class test {
-    @GetMapping("get")
+    @GetMapping("test")
     public String get(){
         return  "Successfully";
     }
