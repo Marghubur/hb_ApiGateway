945c460be7df111bcc592768091f3fda9778e297
istiyakmi9 <istiyaq.mi9@gmail.com>
2024-01-11 13:08:44 +0530

    sb -> 3.0.2

--- src/main/java/com/bot/apigateway/filters/AuthenticationFilter.java
+++ src/main/java/com/bot/apigateway/filters/AuthenticationFilter.java
@@ -7,51 +7,55 @@ import io.jsonwebtoken.security.Keys;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.server.ServerWebExchange;
-import org.springframework.web.server.WebFilter;
-import org.springframework.web.server.WebFilterChain;
-import reactor.core.publisher.Mono;
 
 import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
 import java.util.Objects;
 
 @Component
-@jakarta.servlet.annotation.WebFilter(urlPatterns = "/*")
-public class AuthenticationFilter implements WebFilter {
+public class AuthenticationFilter extends AbstractGatewayFilterFactory<AuthenticationFilter.Config> {
+    public static class Config { }
+
+    public AuthenticationFilter() {
+        super(Config.class);
+    }
     @Autowired
     private RouteValidator routeValidator;
     private final static Logger LOGGER = LoggerFactory.getLogger(AuthenticationFilter.class);
 
     @Override
-    public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
-        LOGGER.info("[API REQUEST]: " + exchange.getRequest().getURI().getPath());
-        if (HttpMethod.OPTIONS.matches(String.valueOf(exchange.getRequest().getMethod()))) {
-            return chain.filter(exchange); // Skip authentication for OPTIONS requests
-        }
-
-        ServerWebExchange modifiedExchange = exchange;
-        if (routeValidator.isSecured.test(exchange.getRequest())) {
-            // check Authorizatrion header -- ServerWebExchange
-            LOGGER.info("[URL REQUESTED]: " + exchange.getRequest().getURI().getPath());
-            if (!exchange.getRequest().getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
-                throw new RuntimeException("Unauthorization access. Token is missing.");
+    public GatewayFilter apply(Config config) {
+        return (((exchange, chain) -> {
+            LOGGER.info("[API REQUEST]: " + exchange.getRequest().getURI().getPath());
+            if (HttpMethod.OPTIONS.matches(String.valueOf(exchange.getRequest().getMethod()))) {
+                return chain.filter(exchange); // Skip authentication for OPTIONS requests
             }
 
-            String authorizationHeader = Objects.requireNonNull(exchange.getRequest().getHeaders().get(HttpHeaders.AUTHORIZATION)).get(0);
-            if (authorizationHeader != null && authorizationHeader.startsWith("Bearer")) {
-                authorizationHeader = authorizationHeader.substring(7);
-            }
+            ServerWebExchange modifiedExchange = exchange;
+            if (routeValidator.isSecured.test(exchange.getRequest())) {
+                // check Authorizatrion header -- ServerWebExchange
+                LOGGER.info("[URL REQUESTED]: " + exchange.getRequest().getURI().getPath());
+                if (!exchange.getRequest().getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
+                    throw new RuntimeException("Unauthorization access. Token is missing.");
+                }
 
-            try {
-                String secret = "bottomhalfx12#@-common-service-for-token-gen-server-ems-x#20$46@3211";
-                byte[] keyBytes = secret.getBytes(StandardCharsets.UTF_8);
-                SecretKey key = Keys.hmacShaKeyFor(keyBytes);
+                String authorizationHeader = Objects.requireNonNull(exchange.getRequest().getHeaders().get(HttpHeaders.AUTHORIZATION)).get(0);
+                if (authorizationHeader != null && authorizationHeader.startsWith("Bearer")) {
+                    authorizationHeader = authorizationHeader.substring(7);
+                }
+
+                try {
+                    String secret = "bottomhalfx12#@-common-service-for-token-gen-server-ems-x#20$46@3211";
+                    byte[] keyBytes = secret.getBytes(StandardCharsets.UTF_8);
+                    SecretKey key = Keys.hmacShaKeyFor(keyBytes);
 
 //                Claims claims = Jwts.parser()
 //                        .setSigningKey(key)
@@ -70,13 +74,14 @@ public class AuthenticationFilter implements WebFilter {
 //                                .build())
 //                        .build();
 
-            } catch (ExpiredJwtException e) {
-                throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Your session got expired");
-            } catch (Exception ex) {
-                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Unauthorized access. Please try with valid token.");
+                } catch (ExpiredJwtException e) {
+                    throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Your session got expired");
+                } catch (Exception ex) {
+                    throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Unauthorized access. Please try with valid token.");
+                }
             }
-        }
 
-        return chain.filter(modifiedExchange);
+            return chain.filter(modifiedExchange);
+        }));
     }
 }
