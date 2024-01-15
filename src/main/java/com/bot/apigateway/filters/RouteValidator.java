975e06937e4580d8fcf075d74f01d8eaf5d2b010
istiyakmi9 <istiyaq.mi9@gmail.com>
2024-01-15 13:15:29 +0530

    image problem resolved

--- src/main/java/com/bot/apigateway/filters/RouteValidator.java
+++ src/main/java/com/bot/apigateway/filters/RouteValidator.java
@@ -9,7 +9,7 @@ import java.util.function.Predicate;
 @Component
 public class RouteValidator {
     public static final List<String> openApiEndpoints = List.of(
-            "/authenticate"
+            "/authenticate", "/registration", "/oauth"
     );
 
     public Predicate<ServerHttpRequest> isSecured =
